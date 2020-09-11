7166d4f8856927970f65c47899285ab280cc3c45
oh <prime89@likelion.org>
2020-09-12 03:12:02 +0900

    4장까지 완료

--- src/main/java/com/prime89/book/springboot/web/PostsApiController.java
+++ src/main/java/com/prime89/book/springboot/web/PostsApiController.java
@@ -1,11 +1,14 @@
 package com.prime89.book.springboot.web;
 
 import com.prime89.book.springboot.service.posts.PostsService;
+import com.prime89.book.springboot.web.dto.PostsListResponseDto;
 import com.prime89.book.springboot.web.dto.PostsResponseDto;
 import com.prime89.book.springboot.web.dto.PostsSaveRequestDto;
 import com.prime89.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -23,8 +26,19 @@ public class PostsApiController {
         return postsService.update(id, requestDto);
     }
 
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+
     @GetMapping("/api/v1/posts/{id}")
-    public PostsResponseDto findById (@PathVariable Long id) {
-        return  postsService.findById(id);
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
+    @GetMapping("/api/v1/posts/list")
+    public List<PostsListResponseDto> findAll() {
+        return postsService.findAllDesc();
     }
 }
